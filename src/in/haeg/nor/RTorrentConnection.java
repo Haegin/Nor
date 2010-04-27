ca1f3a3e54ef0b1ec36f296ef2a16634ad58cc1e
Harry Mills <harry@haeg.in>
2010-04-27 23:01:39 +0100

    Started on the connection class. Currently just copied from the Apache XMLRPC docs on the net.

--- src/in/haeg/nor/RTorrentConnection.java
+++ src/in/haeg/nor/RTorrentConnection.java
@@ -1,5 +1,25 @@
 package in.haeg.nor;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.xmlrpc.client.XmlRpcClient;
+import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
+import org.apache.xmlrpc.client.XmlRpcCommonsTransportFactory;
+
 public class RTorrentConnection {
+    XmlRpcClient           m_Client;
+    XmlRpcClientConfigImpl m_Config;
+
+    public RTorrentConnection(String a_URL) {
+        m_Config = new XmlRpcClientConfigImpl();
+        try {
+            m_Config.setServerURL(new URL(a_URL));
+            m_Client.setTransportFactory(new XmlRpcCommonsTransportFactory(m_Client));
+            m_Client.setConfig(m_Config);
+        } catch (MalformedURLException ex) {
+            ex.printStackTrace();
+        }
+    }
 
 }
