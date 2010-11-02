26903f06673cc26bb748e4fc6623baf0ada8698c
Harry Mills <harry@haeg.in>
2010-11-02 12:42:58 +0000

    Not sure what's changed but I'll commit it anyway

--- src/in/haeg/nor/TorrentFile.java
+++ src/in/haeg/nor/TorrentFile.java
@@ -2,7 +2,7 @@ package in.haeg.nor;
 
 import org.apache.xmlrpc.client.XmlRpcClient;
 
-public class File {
+public class TorrentFile {
 
     private final Object[] FILE_PARAMS;
 
@@ -11,7 +11,7 @@ public class File {
     private Integer        m_FileNumber;
     private XmlRpcClient   m_Connection;
 
-    public File(XmlRpcClient a_Connection, Torrent a_Torrent, String a_FilePath, Integer a_FileNumber) {
+    public TorrentFile(XmlRpcClient a_Connection, Torrent a_Torrent, String a_FilePath, Integer a_FileNumber) {
         m_Connection = a_Connection;
         m_Torrent = a_Torrent;
         m_FilePath = a_FilePath;
